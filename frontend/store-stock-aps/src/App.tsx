88dfeac0ca9962b02d2338b589e3a495c64e0e14
Raul Aquino <aquinoraul207@gmail.com>
2021-05-14 19:29:39 -0300

    Deleting unnecessary files and testing the identity of the front

--- frontend/store-stock-aps/src/App.tsx
+++ frontend/store-stock-aps/src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" className="App-logo"/>
       </header>
     </div>
   );
