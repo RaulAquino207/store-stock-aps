d35adc5bdc6c6ab37108fd76ee13ba93047b7c8f
Raul Aquino <aquinoraul207@gmail.com>
2021-05-22 11:13:35 -0300

    finalized database still being able to receive improvements

--- STORE_STOCK.sql
+++ STORE_STOCK.sql
@@ -3,15 +3,16 @@ CREATE DATABASE store_stock_aps;
 USE store_stock_aps;
 
 CREATE TABLE tbStore
-(store_id INT,
+(store_id INT AUTO_INCREMENT,
 store_name VARCHAR(100) NOT NULL,
 store_owner VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL,
-password VARCHAR(30) NOT NULL
+password VARCHAR(30) NOT NULL,
+PRIMARY KEY (store_id)
 );
 
 CREATE TABLE tbSection
-(section_id INT,
+(section_id INT AUTO_INCREMENT,
 section_name VARCHAR(100) NOT NULL,
 CONSTRAINT pk_tbSection PRIMARY KEY (section_id)
 );
@@ -22,9 +23,22 @@ employee_name VARCHAR(100) NOT NULL,
 email VARCHAR(100) NOT NULL,
 token_login VARCHAR(100),
 
+section_id INT,
 PRIMARY KEY (employee_id),
-FOREIGN KEY (employee_id)
-REFERENCES tbSection (employee_id)
-
+CONSTRAINT fk_tbSection_tbEmployee FOREIGN KEY (section_id)
+REFERENCES tbSection (section_id) ON DELETE CASCADE
+ON UPDATE CASCADE
 );
 
+CREATE TABLE tbProduct
+(product_id INT AUTO_INCREMENT, 
+product_name VARCHAR(100) NOT NULL,
+
+section_id INT,
+PRIMARY KEY (product_id),
+CONSTRAINT fk_tbSection_tbProduct FOREIGN KEY (section_id)
+REFERENCES tbSection (section_id)
+);
+
+#TESTE MY DATABASE
+INSERT INTO tbStore(store_name, store_owner, email, password) VALUES('Love Bag Store', 'Monalisa Sousa Duarte', 'monalisa_drt@outlook.com', '12345678');
