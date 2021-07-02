d2bdff312829dafd12fea99f30c9569923d51d73
Raul Aquino <aquinoraul207@gmail.com>
2021-07-02 00:10:36 -0300

    Added the sidebar component and minor changes to the login screen

---
+++ frontend/src/components/Sidebar/styles.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const StyledNav: any = styled.div``
--- frontend/src/views/LoginPage/index.tsx
+++ frontend/src/views/LoginPage/index.tsx
@@ -72,7 +72,6 @@ const LoginPage: React.FC = () => {
               });
 
               localStorage.setItem("token", reponse.data['token']);
-              localStorage.setItem("token", reponse.data['id']);
               console.log(reponse.data['id']);
               history.push(`/main`);
         } catch(error) {
